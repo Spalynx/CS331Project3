d33880dc2298003ee9e5b345257a236daa5234c3
Spalynx <Spalynx@gmail.com>
2016-04-26 21:24:32 -0500

    C7Q6 branch: Init.

--- src/com/Spal/Main.java
+++ src/com/Spal/Main.java
@@ -3,6 +3,7 @@ package com.Spal;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("FUKKOF");
+        C7_Question6.testClasses();
     }
 }
